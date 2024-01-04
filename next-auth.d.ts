54fdc95b0d008cb1853608cb748b1f74085854e7
kbventures <kmb_x@hotmail.com>
2024-01-04 13:36:28 -0500

    Moved types directory and updated next-auth types

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,12 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line no-unused-vars
-import NextAuth, { DefaultSession } from "next-auth";
+// import NextAuth, { DefaultSession } from "next-auth";
+
+// declare module "next-auth" {
+//     interface Session {
+//         user: {
+//             id: string;
+//             username: string;
+//         } & DefaultSession["user"];
+//     }
+// }
+
+// next-auth.d.ts
+
+import "next-auth";
 
 declare module "next-auth" {
     interface Session {
         user: {
             id: string;
             username: string;
-        } & DefaultSession["user"];
+        };
     }
 }
